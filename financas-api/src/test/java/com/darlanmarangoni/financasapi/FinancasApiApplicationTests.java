f84f8e8abf1e0b5c199481631143e27aa2ea9fad
Darlan Marangoni <marangoni.darlan1@gmail.com>
2024-02-14 18:15:05 -0300

    criando API de financas

---
+++ financas-api/src/test/java/com/darlanmarangoni/financasapi/FinancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darlanmarangoni.financasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
